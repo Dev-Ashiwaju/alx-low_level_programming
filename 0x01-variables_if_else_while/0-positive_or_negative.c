c6660fcd094e934f0cc94259bb59f5e6467015c9
Issa Saheed <olizsurelove@gmail.com>
2022-09-09 04:57:53 -0700

    Positive anything is better than negative nothing

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -3,7 +3,7 @@
 #include <stdio.h>
 
 /**
-* main - Determines if a number randomly generated is positive, negative or zero.
+* main Determines if a number randomly generated is positive, negative or zero.
 *
 * Return: Always 0 (Success)
 */
