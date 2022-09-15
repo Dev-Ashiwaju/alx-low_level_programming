847feed726394daf296410f39d4825e5e7fe5a23
Issa Saheed <olizsurelove@gmail.com>
2022-09-15 16:40:50 -0700

    Fizz-Buzz

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -12,14 +12,14 @@ int main(void)
 
 	for (i = 1; i <= 100; i++)
 	{
-		if ((i % 3) == 0 && (i % 5) == 0)
-			printf("FizzBuzz ");
+		if ((i % 3 == 0) && (i % 5 == 0))
+			printf(" FizzBuzz");
 		else if (i % 5 == 0)
-			printf("Buzz ");
-		else if ((i % 3) == 0)
-			printf("Fizz ");
+			printf(" Buzz");
+		else if (i % 3 == 0)
+			printf(" Fizz");
 		else
-			printf("%d ", i);
+			printf(" %d", i);
 	}
 	printf("\n");
 	return (0);
